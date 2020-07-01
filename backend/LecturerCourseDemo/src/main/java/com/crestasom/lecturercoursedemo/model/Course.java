5c03c3472bea88d651b0b78cfb6a4a7b4ca85fd8
crestasom <crestasom@gmail.com>
2020-07-01 09:14:29 +0545

    many to many mapping for course and semester added

--- backend/LecturerCourseDemo/src/main/java/com/crestasom/lecturercoursedemo/model/Course.java
+++ backend/LecturerCourseDemo/src/main/java/com/crestasom/lecturercoursedemo/model/Course.java
@@ -2,10 +2,13 @@ package com.crestasom.lecturercoursedemo.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -35,7 +38,8 @@ public class Course implements Comparable<Course> {
 	private boolean isSelected;
 	@Transient
 	private boolean visible = true;
-	@ManyToMany
+	@JsonIgnore
+	@ManyToMany(mappedBy = "courseList")
 	private List<Semester> semList;
 
 	@Override
